26a777878dea59d4f57c379e1aeb1b802506e0d3
Kusakabe <103549681+ObiKusakabe@users.noreply.github.com>
2024-01-14 14:52:16 +0700

    Add files via upload

---
+++ function/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_perpusroby";
+
+$koneksi = mysqli_connect($server, $username, $password, $dbname) or die("koneksi gagal"); 
+
+?>
